33d7c654623ccea4a204a037b49547e3797e19d0
Donatas <donataspekelis@gmail.com>
2017-06-27 13:48:00 +0300

    Episode 6
    + Added 2D<->1D map conversion functions

--- math-stuffs.js
+++ math-stuffs.js
@@ -4,3 +4,13 @@ function RandomInt(min, max) // [min; max)
 {
   return min + Math.floor(Math.random() * max);
 }
+
+function xy2i(x, y, map_width)
+{
+  return y * map_width + x;
+}
+
+function i2xy(i, map_width)
+{
+  return [ (i % map_width), (Math.floor(i / map_width)) ];
+}
